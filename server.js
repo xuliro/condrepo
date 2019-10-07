37b93d9ea09d6ef8f0a03d0498e2e3407a46eabc
Ulisses Rodrigues <ulisses0205@gmail.com>
2019-10-07 17:20:08 -0300

    Update server.js

--- server.js
+++ server.js
@@ -3,6 +3,6 @@ var http = require ('http');
 var app = require('./config/express')(app);
 require('./config/passport')();
 require('./config/database.js')('mongodb://condrepo:Condrepo_123@ds331568.mlab.com:31568/heroku_hdrqdmfd');
-http.createServer(app).listen(app.get('port'), function(){    
+http.createServer(app).listen(process.env.PORT || 3000), function(){    
     console.log('Server escutando a porta ' + app.get('port'));
 });
