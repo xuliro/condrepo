23386f560b31f1db6ab221b301d790bec7805ff1
Ulisses Rodrigues <ulisses0205@gmail.com>
2019-10-07 17:58:45 -0300

    Update server.js

--- server.js
+++ server.js
@@ -6,5 +6,5 @@ require('./config/passport')();
 require('./config/database.js')('mongodb://condrepo:Condrepo_123@ds331568.mlab.com:31568/heroku_hdrqdmfd');
 const PORT = process.env.PORT || 3000;
 http.createServer(app).listen(PORT, () => {
-    console.log(`Our app is running on port ${ PORT }`);
+    console.log(`Server rundando na porta ${ PORT }`);
 });
